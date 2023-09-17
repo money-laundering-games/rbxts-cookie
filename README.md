f216607246f54beb3db8e0f86b785508a1517e0d
chloegit138 <chloewatkins138@gmail.com>
2023-09-17 18:44:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# rbxts-cookie
+
+roblox-ts cookie parser. rewritten in ts from [cookie.lua](https://github.com/cyx/cookie.lua/)
+
+## Usage
+```ts
+import { parse, build } from "@money-laundering-games/rbxts-cookie";
+
+// basic usage
+assert("foo=bar" === build({foo: "bar"}));
+
+// using all possible option values
+
+const time = 1398283214;
+const options = {
+    'max_age': 3600,
+    'domain': ".example.com",
+    'path': "/",
+    'expires': time,
+    'http_only': true,
+    'secure': true
+};
+
+const expected =
+	"foo=bar; Max-Age=3600; Domain=.example.com; " +
+	"Path=/; Expires=Wed, 23 Apr 2014 13:01:14 GMT; " +
+	"HttpOnly; Secure";
+
+assert(expected === build({foo: "bar"}, options));
+
+// parsing
+
+assert(equal({ foo:"bar", bar:"baz" },
+	parse("foo=bar; bar=baz")));
+
+assert(equal({ foo:"bar", bar:"baz" },
+	parse('foo=bar; bar="baz"')));
+
+assert(equal({ foo:"bar", bar:"baz" },
+	parse('foo=bar; bar="baz"; foo=baz')));
+
+assert(equal({ foo:"bar", bar:"baz" },
+	parse('foo=bar; bar="baz"; barabaz')));
+
+```
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+const format = string.format;
+const gmatch = string.gmatch;
+const sub = string.sub;
+const gsub = string.gsub;
+const char = string.char;
+const find = string.find;
+const HttpService = game.GetService("HttpService");
+
+function tonumberproxy(...args: [string, number]): number {
+	return tonumber(args)!;
+}
+
+function decodeChar(hex: string) {
+	return char(tonumberproxy(hex, 16));
+}
+
+function decodeString(str: string) {
+	const [output, t] = gsub(str, "%%(%x%x)", decodeChar);
+	return output;
+}
+
+const identity = function <T>(toReturn: T): T {
+	return toReturn;
+};
+
+const clean = function (str: string): string {
+	let [s] = gsub(str, "^%s*(.-)%s*$", "%1");
+
+	if (sub(s, 1, 1) === '"') {
+		s = sub(s, 2, -2);
+	}
+
+	return s;
+};
+
+const to_utc_string = function (time: number) {
+	return os.date("%a, %d %b %Y %H:%I:%S GMT", time);
+};
+
+type CODEX_TYPE = [string, string, Callback][];
+
+const CODEX: CODEX_TYPE = [
+	["max_age", "Max-Age=%d", identity],
+	["domain", "Domain=%s", identity],
+	["path", "Path=%s", identity],
+	["expires", "Expires=%s", to_utc_string],
+	["http_only", "HttpOnly", identity],
+	["secure", "Secure", identity],
+];
+
+export const build = function (dict: Map<string, string>, options: Record<string, number | string | boolean>) {
+	const res = [];
+
+	for (const [k, v] of dict) {
+		res.push(format("%s=%s", k, HttpService.UrlEncode(v)));
+	}
+
+	for (const [key, template, fn] of CODEX) {
+		const val = options[key];
+		if (val !== undefined) {
+			res.push(format(template, fn(val)));
+		}
+	}
+
+	return res.join("; ");
+};
+
+export const parse = function (data: string) {
+	const res: Map<string, string> = new Map<string, string>();
+
+	for (const pair of gmatch(data, "[^;]+")) {
+		const eq = find(pair[0] as string, "=")[0];
+
+		if (eq !== undefined) {
+			const key = clean(sub(pair[0] as string, 1, eq - 1));
+			const val = clean(sub(pair[0] as string, eq + 1));
+
+			if (res.get(key) === undefined) {
+				res.set(key, decodeString(val));
+			}
+		}
+	}
+
+	return res;
+};
+
+export default {
+	build: build,
+	parse: parse,
+};
